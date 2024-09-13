a1dfba65f04a9ba0f1dcf4f4a922e8e1db45301e
anobo2107 <nastasia.arch@gmail.com>
2024-09-13 20:17:17 +0300

    Corrected_HW_Fin

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,65 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
-    }
-}
+            System.out.println("Тask1");
+            for (int i = 1; i <= 10; i++) {
+                System.out.println("иттерация цикла"+ i);
+            }
+
+            System.out.println("Тask2");
+                for (int i = 10; i >= 1; i--) {
+                    System.out.println(i);
+                }
+
+            System.out.println("Тask3");
+                    for (int i = 0; i <= 17; i += 2) {
+                        System.out.println( "иттерация цикла"+ i);
+                    }
+
+            System.out.println("Тask4");
+                    for (int i = 10; i <= 10 && i >= -10; i--) {
+                    System.out.println(i);
+                        }
+            System.out.println("Тask5");
+                    for (int i = 1904; i <= 2096; i += 4) {
+                    System.out.println(i + "год является високосным");
+            }
+
+            System.out.println("Тask6");
+                    for (int i = 7; i <= 98; i += 7) {
+                    System.out.println(i);
+            }
+
+            System.out.println("Тask7");
+                    for (int i = 1; i <= 512; i *= 2) {
+                    System.out.println(i);
+            }
+
+        {
+            System.out.println("Тask8 ");
+            int monthlyDeposit = 29000;
+            int totalSavings = 0;
+            for (int month = 1; month <= 12; month++) {
+            totalSavings += monthlyDeposit;
+                System.out.println("Месяц " + month + ", сумма накоплений равна " + totalSavings + " рублей");
+            }
+
+
+            System.out.println("Тask9 ");
+            int deposit = 29000;
+            int totalDeposit = 0;
+            int monthPercent= 1;
+            for (int i = 1; i <= 12; i++) {
+            totalDeposit += deposit;
+            totalDeposit += totalDeposit * monthPercent / 100;
+            System.out.println("Месяц " + i + ", сумма накоплений равна " + totalDeposit + " рублей");
+            }
+
+            System.out.println("Тask10");
+            int n = 2;
+            for (int k = 1; k <= 10; k++) {
+            System.out.println(n + " * " + k + " = " + (n * k));
+            }
+        }}}
+        ;
+
