211eee51ca0aa3fe3ffdc76d416abb9cd21c616b
anobo2107 <nastasia.arch@gmail.com>
2024-09-15 12:48:13 +0300

    Corrected_HW_Fin_3

--- src/Main.java
+++ src/Main.java
@@ -1,46 +1,46 @@
 public class Main {
     public static void main(String[] args) {
 
-            System.out.println("Тask1");
-            for (int i = 1; i <= 10; i++) {
-                System.out.println("иттерация цикла"+ i);
-            }
+        System.out.println("Тask1");
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("иттерация цикла" + i);
+        }
 
-            System.out.println("Тask2");
-                for (int i = 10; i >= 1; i--) {
-                    System.out.println(i);
-                }
+        System.out.println("Тask2");
+        for (int i = 10; i >= 1; i--) {
+            System.out.println(i);
+        }
 
-            System.out.println("Тask3");
-                    for (int i = 0; i <= 17; i += 2) {
-                        System.out.println( "иттерация цикла"+ i);
-                    }
+        System.out.println("Тask3");
+        for (int i = 0; i <= 17; i += 2) {
+            System.out.println("иттерация цикла" + i);
+        }
 
-            System.out.println("Тask4");
-                    for (int i = 10; i <= 10 && i >= -10; i--) {
-                    System.out.println(i);
-                        }
-            System.out.println("Тask5");
-                    for (int i = 1904; i <= 2096; i += 4) {
-                    System.out.println(i + "год является високосным");
-            }
+        System.out.println("Тask4");
+        for (int i = 10; i <= 10 && i >= -10; i--) {
+            System.out.println(i);
+        }
+        System.out.println("Тask5");
+        for (int i = 1904; i <= 2096; i += 4) {
+            System.out.println(i + "год является високосным");
+        }
 
-            System.out.println("Тask6");
-                    for (int i = 7; i <= 98; i += 7) {
-                    System.out.println(i);
-            }
+        System.out.println("Тask6");
+        for (int i = 7; i <= 98; i += 7) {
+            System.out.println(i);
+        }
 
-            System.out.println("Тask7");
-                    for (int i = 1; i <= 512; i *= 2) {
-                    System.out.println(i);
-            }
+        System.out.println("Тask7");
+        for (int i = 1; i <= 512; i *= 2) {
+            System.out.println(i);
+        }
 
         {
             System.out.println("Тask8 ");
             int monthlyDeposit = 29000;
             int totalSavings = 0;
             for (int month = 1; month <= 12; month++) {
-            totalSavings += monthlyDeposit;
+                totalSavings += monthlyDeposit;
                 System.out.println("Месяц " + month + ", сумма накоплений равна " + totalSavings + " рублей");
             }
 
@@ -48,19 +48,21 @@ public class Main {
             System.out.println("Тask9 ");
             int deposit = 29000;
             double totalDeposit = 0;
-            double monthPercent= 1;
+            double monthPercent = 1;
             for (int i = 1; i <= 12; i++) {
-            totalDeposit += deposit;
-            totalDeposit += totalDeposit * monthPercent / 100;
-            System.out.printf ("Месяц %d, сумма накоплений равна %.2f рублей%n",i, totalDeposit);
+                totalDeposit += deposit;
+                totalDeposit += totalDeposit * monthPercent / 100;
+                System.out.printf("Месяц %d, сумма накоплений равна %.2f рублей%n", i, totalDeposit);
             }
 
 
             System.out.println("Тask10");
             int n = 2;
             for (int k = 1; k <= 10; k++) {
-            System.out.println(n + " * " + k + " = " + (n * k));
+                System.out.println(n + " * " + k + " = " + (n * k));
             }
-        }}}
-        ;
+        }
+    }
+}
+;
 
